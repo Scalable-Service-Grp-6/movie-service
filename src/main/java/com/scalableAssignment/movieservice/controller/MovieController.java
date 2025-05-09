90b3ce88415fc7bb0d2afd7143b23b4b9a5926a6
Lakshmi-Pravallika <bnlpravallika@gmail.com>
2025-05-09 22:58:40 +0530

    Auth impl

--- src/main/java/com/scalableAssignment/movieservice/controller/MovieController.java
+++ src/main/java/com/scalableAssignment/movieservice/controller/MovieController.java
@@ -7,36 +7,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scalableAssignment.movieservice.model.Movie;
 import com.scalableAssignment.movieservice.service.MovieService;
+import org.springframework.http.HttpHeaders;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
 
-    private final MovieService movieService;
+	private final MovieService movieService;
 
-    public MovieController(MovieService movieService) {
-        this.movieService = movieService;
-    }
+	public MovieController(MovieService movieService) {
+		this.movieService = movieService;
+	}
 
-    @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies() {
-        return ResponseEntity.ok(movieService.getAllMovies());
-    }
+	@GetMapping
+	public ResponseEntity<List<Movie>> getAllMovies() {
+		return ResponseEntity.ok(movieService.getAllMovies());
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Movie> getMovieById(@PathVariable(name = "id") String id) {
-        return movieService.getMovieById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<Movie> getMovieById(@PathVariable(name = "id") String id) {
+		return movieService.getMovieById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	}
 
-    @PostMapping
-    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
-        return ResponseEntity.ok(movieService.addMovie(movie));
-    }
+	@PostMapping
+	public ResponseEntity<Movie> addMovie(@RequestBody Movie movie,
+			@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+		return ResponseEntity.ok(movieService.addMovie(movie, authHeader));
+	}
 }
--- src/main/java/com/scalableAssignment/movieservice/service/MovieService.java
+++ src/main/java/com/scalableAssignment/movieservice/service/MovieService.java
@@ -1,9 +1,13 @@
 package com.scalableAssignment.movieservice.service;
 
+import org.springframework.http.*;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.scalableAssignment.movieservice.model.Movie;
 import com.scalableAssignment.movieservice.repository.MovieRepository;
@@ -11,6 +15,13 @@ import com.scalableAssignment.movieservice.repository.MovieRepository;
 @Service
 public class MovieService {
 
+	
+	@Value("${use_auth}")
+    private boolean useAuth;
+	
+	@Value("${auth_url}")
+    private String auth_url;
+	
     private final MovieRepository movieRepository;
 
     public MovieService(MovieRepository movieRepository) {
@@ -25,7 +36,23 @@ public class MovieService {
         return movieRepository.findById(id);
     }
 
-    public Movie addMovie(Movie movie) {
+    public Movie addMovie(Movie movie, String token) {
+    	if(useAuth) {
+    		System.out.println("Auth required...");
+    		System.out.println("Calling "+auth_url);
+    		RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(token); 
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            try {
+                ResponseEntity<String> response = restTemplate.exchange(auth_url, HttpMethod.GET, entity, String.class);
+                if (response.getStatusCode() != HttpStatus.OK) {
+                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You are not authorized to add movies");
+                }
+            } catch (Exception e) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You are not authorized to add movies");
+            }
+    	}
         return movieRepository.save(movie);
     }
 }
